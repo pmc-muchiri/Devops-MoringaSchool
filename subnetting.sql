9ccb67dbfa2e91e5f799f0fbaabe85b6bc4dcb1b
pmc_admin_relational <pmc.ac.ke@gmail.com>
2025-09-11 16:53:08 +0300

    edits document

--- subnetting.sql
+++ subnetting.sql
@@ -32,16 +32,16 @@ c)   network ip: From our IP 156.54.1.24 we will convert to binary
 
 	 	Convert to Decimal: 156.54.0.0/16
 
-	 Network Ip has zeros in the last octet of the host portion(the first IP from the subnet)
+	 Network Ip has zeros  the last octet of the host portion(the first IP from the subnet)
 
 d)broadcastip 
 	 From the subnet mask 11111111.11111111|.00000000.00000000 first 16 represent Network portion(unchanged) and the remaning rep host portion(can change)
-	 from the class broadcastip is the last network so the host portion of our ip should have all 1s to be the last network broadcastip
+	 from the class broadcastip is the last network so the host portion of our ip should have all 1s to be the last network broadcastip(13)
 	 10011100.00110110 .00000000.00000000 --> 10011100.00110110 .11111111.11111111
 
 	 convert to Decimal :156.54.255.255 
 
-	 Broadcast IP has all 1s at on the last octet of the host portion.
+	 Broadcast IP has all 1s  the last octet of the host portion.
 
 				
 e)	first usable address : Network IP +1 
@@ -57,7 +57,7 @@ f) last usable address  : Broadcast IP - 1
 2. Create 6 subnets from the IP and give the same details as above for all subnet ranges
 
 We need to know number of bits to be added to our subnet mask
-from this formular 2^n=Network we can say n -s the bits
+from this formular 2^n=Network we can say n is the bits
 
  Bits						Network                  
 ===============================================
@@ -81,16 +81,16 @@ From the required subnets 6 we will check where these 6 networks can fit and tak
 For our new subnet mask we will add 3 bits to the previous subnet mask to get /19 for each subnet.
 this will generate 2^(32-19) =8192 addrs
 
-1. First Subnet
+Calculate Subnets
 
 Previous Subnet : 11111111.11111111|00000000.00000000 ---> 255.255.0.0 
-New Subnet      : 11111111.11111111.111|00000.00000000 ---> 255.255.0.0 ( 128 	64		32		16 		8 		4 		2 		1)using the third octet since others are know
-																			1    1 		 1       0      0       0       0       0 = 128+64+32 = 224
+New Subnet      : 11111111.11111111.111|00000.00000000 ---> 255.255.224.0 ( 128 	64		32		16 		8 		4 		2 		1)using the third octet since others are know
+																			  1      1 		 1       0      0       0       0       0 = 128+64+32 = 224
 													  ---> 255.255.224.0
 
-
-	IP   		: 10011100.00110110|000|00001.00011000
-	Subnet mask : 11111111.11111111|111|00001.00000000 --->255.255.224.0
+1. 1st Subnet:
+	IP   		: 10011100.00110110|000|00001.00011000 --->156.54.1.24
+ 	Subnet mask : 11111111.11111111|111|00001.00000000 --->255.255.224.0
 	Network IP  : 10011100.00110110|000 00000.00000000 --->156.54.0.0
 	broadcastip : 10011100.00110110|000 11111.11111111 --->156.54.31.255 ( 128 	64		32		16 		8 		4 		2 		1)
 																			0    0       0        1     1       1       1       1 = 16+8+4+2+1 = 31
